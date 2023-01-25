a82df4efbeeba37ef90d30dcf4a421edd8e90daa
Your Name <you@example.com>
2023-01-25 10:53:18 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import "./Root.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import OurBenefits from "./components/OurBenefits";
 import Features from "./components/Features";
 import StayConnect from "./components/StayConnect";
+import Global from "./components/Global";
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <OurBenefits />
       <Features />
       <StayConnect />
+      <Global />
     </>
   );
 }
