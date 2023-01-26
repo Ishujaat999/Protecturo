feb624a3c23248192b8100613b5da56304cabff7
kapil verma <vermakp39@gmail.com>
2023-01-26 09:58:38 -0800

    ok

--- src/components/SliderDummy.jsx
+++ src/components/SliderDummy.jsx
@@ -7,7 +7,7 @@ const SliderDummy = () => {
     dots: true,
     arrows: false,
     infinite: true,
-    autoplay: true,
+    autoplay: false ,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -24,7 +24,7 @@ const SliderDummy = () => {
               <h2 className=" fw-bold fs_xxl_30 tc_danger fs_sm_25">
                 {obj.heading}
               </h2>
-              <p className="tc_info">{obj.para}</p>
+              <p className="tc_info fs_xsm ">{obj.para}</p>
             </div>
           ))}
         </Slider>
